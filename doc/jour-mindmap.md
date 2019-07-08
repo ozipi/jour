9e0be46d0df300eec212db026291f3061dc748e2
Oscar Valdez <oscar@ozipi.dev>
2019-07-07 20:09:37 -0500

    [docs] added mindmap with different file formats to test on github

---
+++ doc/jour-mindmap.md
@@ -0,0 +1,15 @@
+# jour project
+
+
+## definition
+
+- project activities time tracker
+
+## jour
+
+- dashboard reporting tool using react/d3
+
+## jour-cli
+
+- jour cli for time reporting as a react/ink/form or directly from cli params
+
---
+++ doc/jour-mindmap.mm
@@ -0,0 +1,8 @@
+<map version="1.0.0"><node ID="A1B86A86-BCDA-4BF0-A83D-F4210F95390B" BACKGROUND_COLOR="#040403" TEXT="jour project" COLOR="#F4FFE3" POSITION="right" STYLE="bubble"><edge COLOR="#F4FFE2" WIDTH="4" /><font NAME="HelveticaNeue-Thin" SIZE="24" /><node ID="6C3DBD0E-AC35-481F-8C8B-6292D922DE32" TEXT="definition" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#68FF6D" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /><node ID="5A347281-13BC-475B-B333-D693071CFB23" TEXT="project activities time tracker" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#68FF6D" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /></node>
+</node>
+<node ID="C2D3ECFE-B7A6-418A-82EB-1092C93CD939" TEXT="jour" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#FCB114" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /><node ID="DE3F2A26-2662-402A-B9FE-7FEB6890EB63" TEXT="dashboard reporting tool using react/d3" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#FCB114" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /></node>
+</node>
+<node ID="4C67B6FA-CFF1-4386-AFA7-DEC4D1E05A0F" TEXT="jour-cli" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#00E0A1" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /><node ID="9125D048-E3B5-4BAE-9901-DA40EA08A4B4" TEXT="jour cli for time reporting as a react/ink/form or directly from cli params" COLOR="#FFFEE0" POSITION="right" STYLE="fork"><edge COLOR="#00E0A1" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="21" /></node>
+</node>
+</node>
+</map>
